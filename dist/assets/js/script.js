393647219488253b778629e5b53d5b1e0c69747b
romantut1988 <romantut1988@mail.ru>
2021-08-08 01:11:31 +0300

    adding dimensions to the map section

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -2,12 +2,25 @@ function test(a, b) {
     return a + b
 }
 
-$(document).ready(function(){
-    $('.single-item').slick({
-      dots: true
-    });
-  });
-      
+// $(document).ready(function(){
+//     $('.single-item').slick({
+//       dots: true
+//     });
+//   });
 
-  
+// import Swiper from "swiper";
+// import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+
+// var swiper = new Swiper(".mySwiper", {
+//   cssMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
 //# sourceMappingURL=script.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,21 @@
-$(document).ready(function(){
-    $('.single-item').slick({
-      dots: true
-    });
-  });
-      
+// $(document).ready(function(){
+//     $('.single-item').slick({
+//       dots: true
+//     });
+//   });
 
-  
+// import Swiper from "swiper";
+// import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+
+// var swiper = new Swiper(".mySwiper", {
+//   cssMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
